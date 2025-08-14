fa30d7c8d4d64b7f00f4bea8d7ec90c9f9f210c3
Suhel Naryal <suhelnaryal@gmail.com>
2025-08-14 16:47:39 +0530

    Added fix for prod deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/pc-to-ear/' : '/',
   server: {
     host: "::",
     port: 8080,
